68640d7a3abf2ed0735e83028600ea7f4077f62f
ettorepuccetti <140594>
2023-02-10 17:13:52 +0100

    deleting dataContext as causing build fail (?)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import type { AppProps } from 'next/app'
-import { DataProvider } from '../components/DataContext';
 import '@picocss/pico';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DataProvider>
-      <Component {...pageProps} />
-    </DataProvider>
+    <Component {...pageProps} />
   ) 
 }
